8b0998436756ff3a32399a06eaffbd78a62ffac4
Jason <2594338376@qq.com>
2022-11-23 01:58:04 +0800

    vue实现版本1.0

---
+++ TodoList-version1.0/src/components/MyHeader.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="todo-header">
+    <input type="text" placeholder="请输入你的任务名称，按回车键确认" v-model="title"  @keyup.enter="add"/>
+  </div>
+</template>
+
+<script>
+//使用nanoid生成不重复id
+import {nanoid} from 'nanoid'
+
+export default {
+  name: "MyHeader",
+  props: ['addTodo'],
+  data(){
+    return{
+      title:''
+    }
+  },
+  methods: {
+    add(e) {
+      //校验数据
+      if(!this.title.trim()){
+        return
+      }
+      //将用户的输入包装成为一个todo对象
+      const todoObj = {id: nanoid(), title: this.title, done: false}
+      this.addTodo(todoObj)
+      this.title = ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+/*header*/
+.todo-header input {
+  width: 560px;
+  height: 28px;
+  font-size: 14px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  padding: 4px 7px;
+}
+
+.todo-header input:focus {
+  outline: none;
+  border-color: rgba(82, 168, 236, 0.8);
+  box-shadow: inset 0 1px 1px rgba(0, 0, 0, 0.075), 0 0 8px rgba(82, 168, 236, 0.6);
+}
+</style>
